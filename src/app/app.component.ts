d96d93a02688b00f971b5c4601ab4912d3c26b7e
Beruto <beruto000@gmail.com>
2018-01-30 01:05:48 +0100

    Switched to FandF

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import 'rxjs/add/operator/filter';
 import { DOCUMENT } from '@angular/platform-browser';
 import { LocationStrategy, PlatformLocation, Location } from '@angular/common';
 import { NavbarComponent } from './shared/navbar/navbar.component';
+import {version} from "punycode";
 
 @Component({
     selector: 'app-root',
@@ -14,14 +15,13 @@ import { NavbarComponent } from './shared/navbar/navbar.component';
 export class AppComponent implements OnInit {
     private _router: Subscription;
     @ViewChild(NavbarComponent) navbar: NavbarComponent;
-
-    constructor( private renderer : Renderer, private router: Router, @Inject(DOCUMENT,) private document: any, private element : ElementRef, public location: Location) {}
+constructor( private renderer: Renderer, private router: Router, @Inject(DOCUMENT, ) private document: any, private element: ElementRef, public location: Location) {}
     ngOnInit() {
-        var navbar : HTMLElement = this.element.nativeElement.children[0].children[0];
+        const navbar: HTMLElement = this.element.nativeElement.children[0].children[0];
         this._router = this.router.events.filter(event => event instanceof NavigationEnd).subscribe((event: NavigationEnd) => {
             if (window.outerWidth > 991) {
                 window.document.children[0].scrollTop = 0;
-            }else{
+            }else {
                 window.document.activeElement.scrollTop = 0;
             }
             this.navbar.sidebarClose();
@@ -36,28 +36,23 @@ export class AppComponent implements OnInit {
                 navbar.classList.add('navbar-transparent');
             }
         });
-        var ua = window.navigator.userAgent;
-        var trident = ua.indexOf('Trident/');
+        const ua = window.navigator.userAgent;
+        const trident = ua.indexOf('Trident/');
         if (trident > 0) {
             // IE 11 => return version number
-            var rv = ua.indexOf('rv:');
-            var version = parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+            const rv = ua.indexOf('rv:');
+            const version = parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
         }
         if (version) {
-            var body = document.getElementsByTagName('body')[0];
+            let body = document.getElementsByTagName('body')[0];
             body.classList.add('ie-background');
 
         }
 
     }
     removeFooter() {
-        var titlee = this.location.prepareExternalUrl(this.location.path());
+        let titlee = this.location.prepareExternalUrl(this.location.path());
         titlee = titlee.slice( 1 );
-        if(titlee === 'signup' || titlee === 'nucleoicons'){
-            return false;
-        }
-        else {
-            return true;
-        }
+        return titlee === 'signup' || titlee === 'nucleoicons';
     }
 }
