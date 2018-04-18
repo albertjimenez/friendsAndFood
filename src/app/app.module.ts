4e196af8f3270101721774870ca7c9492e1f1525
Beruto <beruto000@gmail.com>
2018-04-18 16:48:22 +0200

    goTop button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { RouterModule } from '@angular/router';
-import { AppRoutingModule } from './app.routing';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {RouterModule} from '@angular/router';
+import {AppRoutingModule} from './app.routing';
 
-import { AppComponent } from './app.component';
-import { SignupComponent } from './signup/signup.component';
-import { LandingComponent } from './landing/landing.component';
-import { ProfileComponent } from './profile/profile.component';
-import { HomeComponent } from './home/home.component';
-import { NavbarComponent } from './shared/navbar/navbar.component';
-import { FooterComponent } from './shared/footer/footer.component';
+import {AppComponent} from './app.component';
+import {SignupComponent} from './signup/signup.component';
+import {LandingComponent} from './landing/landing.component';
+import {ProfileComponent} from './profile/profile.component';
+import {NavbarComponent} from './shared/navbar/navbar.component';
+import {FooterComponent} from './shared/footer/footer.component';
 
-import { HomeModule } from './home/home.module';
+import {HomeModule} from './home/home.module';
+import {ScrollEventModule} from 'ngx-scroll-event';
 
 @NgModule({
   declarations: [
@@ -30,7 +30,7 @@ import { HomeModule } from './home/home.module';
     FormsModule,
     RouterModule,
     AppRoutingModule,
-    HomeModule
+      HomeModule, ScrollEventModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ScrollEvent} from "ngx-scroll-event";
 
 @Component({
     selector: 'app-landing',
@@ -8,8 +9,32 @@ import { Component, OnInit } from '@angular/core';
 
 export class LandingComponent implements OnInit {
 
+    goTop = false;
+    goBot = false;
   constructor() { }
 
   ngOnInit() {}
+
+    public handleScroll(event: ScrollEvent) {
+        if (event.isReachingBottom) {
+            console.log('Abajo');
+            this.goTop = true;
+            this.goBot = false;
+        } else if (event.isReachingTop) {
+            console.log('Arriba');
+            this.goTop = false;
+            this.goBot = !this.goBot;
+        }
+
+    }
+
+    goToTheTop() {
+        window.scroll(0, 0);
+    }
+
+    goToTheBottom() {
+        window.scroll(0, 3000);
+    }
+
 
 }
